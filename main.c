e44153277c384985a0e4c5e12d9d58323bbebc2e
vincent <vincent@annanas.ca>
2020-10-18 08:13:00 +0100

    Simple example that load on shift register with hardcoded pins

--- main.c
+++ main.c
@@ -1,10 +1,56 @@
 #define F_CPU 1000000L
 #include <avr/io.h>
 #include <util/delay.h>
-#include <shift_register_8.h>
+//#include <shift_register_8.h>
+
+#define DATA PB0
+#define LATCH PB2
+#define CLOCK PB3
+
+unsigned char push8(unsigned char);
 
 int main(void)
 {
+    DDRB |= (1 << DATA);
+    DDRB |= (1 << LATCH);
+    DDRB |= (1 << CLOCK);
 
+    uint8_t test =0;
+    test++;
+    
+    unsigned char sample = 15;
+    while(1)
+    {
+        sample = 1;
+        for(int i = 0; i <= 7; i++)
+        {
+            push8(sample);
+            sample = sample*2 + 1; //bar
+            //sample++; //incremental
+            //sample *= 2; //chenillard
+            _delay_ms(50);
+            
+        }
+    }
 return 0;
+}
+
+unsigned char push8(unsigned char bte)
+{            
+    PORTB &= ~(1 << LATCH); // Let's lock the output
+
+    for(int idx = 0; idx <= 7; idx++)
+    {
+        PORTB &= ~(1 << CLOCK); // Clock to low state before adding data
+
+        if (bte & 0b00000001)
+            PORTB |= (1 << DATA);  // if LSB is equal to 1 let's add it to port B
+        else
+            PORTB &= ~(1 << DATA);  // otherwise, we put to 0
+
+        PORTB |= (1 << CLOCK);  // Clock set to high, edge will push the bit
+        bte >>= 1;              // Shift to next bit in the 8 bit given
+    }
+    PORTB |= (1 << LATCH);      // When all bits have been sent, release the lock
+    return 1;
 }
--- shift_register_8.c
+++ shift_register_8.c
@@ -1,6 +1,8 @@
+// Blue : Data - PB0
+// grey : latch - PB2
+// purple : clock - PB3
 
-
-uint8 push8(unsigned char word)
+uint8 push8(uint8 bte)
 {
 
 
